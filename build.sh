d306e8a3a4b2105e8c483024775f56bae90691d1
linrongbin <linrongbin16@gmail.com>
2019-04-09 14:20:44 +0800

    update

--- build.sh
+++ build.sh
@@ -1,16 +1,13 @@
 #! /usr/bin/env bash
 
 echo ""
-echo ""
 echo "[way-to-algorithm-latex] latex main.tex"
 latex main.tex
 
 echo ""
-echo ""
 echo "[way-to-algorithm-latex] dvips main.ps"
 dvips main.dvi
 
-echo ""
 echo ""
 echo "[way-to-algorithm-latex] ps2pdf main.ps"
 ps2pdf main.ps
---
+++ clear.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+echo ""
+echo "[way-to-algorithm-latex] rm files"
+rm -rf *.dvi *.pdf *.ps *.aux *.log 2>/dev/null 1>&2
