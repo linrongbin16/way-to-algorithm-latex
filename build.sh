d77cbc3a1f5e29366cf619fc824f3212648e8d29
linrongbin <linrongbin16@gmail.com>
2019-04-11 21:26:57 +0800

    add cite

--- build.sh
+++ build.sh
@@ -3,3 +3,13 @@
 echo ""
 echo "[way-to-algorithm-latex] xelatex wta.tex"
 xelatex wta.tex
+
+
+echo ""
+echo "[way-to-algorithm-latex] bibtex wta.aux"
+bibtex wta.aux
+
+echo ""
+echo "[way-to-algorithm-latex] xelatex wta.tex twice"
+xelatex wta.tex
+xelatex wta.tex
