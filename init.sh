28f6d77819ae866f94203c60574dfb5a0e728003
linrongbin <linrongbin16@gmail.com>
2019-04-11 20:13:31 +0800

    add init.sh

---
+++ init.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+echo ""
+echo "[way-to-algorithm-latex] git clone https://github.com/linrongbin16/way-to-algorithm-image wta-image"
+git clone https://github.com/linrongbin16/way-to-algorithm-image wta-image
