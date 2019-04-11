860cc29fb62e0dee9a31a36d5e432be0777ec161
linrongbin <linrongbin16@gmail.com>
2019-04-11 17:14:38 +0800

    update book

--- build.sh
+++ build.sh
@@ -3,11 +3,3 @@
 echo ""
 echo "[way-to-algorithm-latex] xelatex wta.tex"
 xelatex wta.tex
-
-echo ""
-echo "[way-to-algorithm-latex] dvips wta.ps"
-dvips wta.dvi
-
-echo ""
-echo "[way-to-algorithm-latex] ps2pdf wta.ps"
-ps2pdf wta.ps
