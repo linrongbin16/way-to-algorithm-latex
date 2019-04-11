85e0baf6ad184a2e5738722b469f896e8c837c93
linrongbin <linrongbin16@gmail.com>
2019-04-11 21:37:37 +0800

    update build clear scripts, use white for hyperref

--- clear.sh
+++ clear.sh
@@ -1,5 +1,23 @@
 #! /usr/bin/env bash
 
+quiet_rm() {
+    rm -rf $@ 2>/dev/null 1>&2
+}
+
 echo ""
 echo "[way-to-algorithm-latex] remove binary files"
-rm -rf *.dvi *.pdf *.ps *.aux *.log *.fdb_latexmk *.fls *.synctex* *.out *.thm *.toc 2>/dev/null 1>&2
+quiet_rm *.dvi
+quiet_rm *.pdf
+quiet_rm *.ps
+quiet_rm *.aux
+quiet_rm *.log
+quiet_rm *.fdb_latexmk
+quiet_rm *.fls
+quiet_rm *.synctex*
+quiet_rm *.out
+quiet_rm *.thm
+quiet_rm *.toc
+quiet_rm *.toc
+quiet_rm __latexindent_temp.tex
+quiet_rm *.bbl
+quiet_rm *.blg
