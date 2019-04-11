064934a561cdd0b87a11776c322fcf7483b46aa0
linrongbin <linrongbin16@gmail.com>
2019-04-11 15:34:54 +0800

    udpate doc

--- clear.sh
+++ clear.sh
@@ -2,4 +2,4 @@
 
 echo ""
 echo "[way-to-algorithm-latex] remove binary files"
-rm -rf *.dvi *.pdf *.ps *.aux *.log *.fdb_latexmk *.fls *.synctex* *.out 2>/dev/null 1>&2
+rm -rf *.dvi *.pdf *.ps *.aux *.log *.fdb_latexmk *.fls *.synctex* *.out *.thm 2>/dev/null 1>&2
