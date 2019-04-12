23be5bd40f88383a838e64998fdad29387bf2642
linrongbin <linrongbin16@gmail.com>
2019-04-12 12:15:56 +0800

    add build, clear script for windows

---
+++ build.bat
@@ -0,0 +1,33 @@
+@echo off
+
+:check_exit_code
+if %errorlevel% NEQ 0 (
+    echo "[way-to-algorithm-latex] Error!"
+    exit /b %errorlevel%
+)
+goto:eof
+
+if not exist wta-image (
+    echo ""
+    echo "[way-to-algorithm-latex] git submodule update --init --recursive"
+    git submodule update --init --recursive
+    git submodule update --recursive
+    call:check_exit_code
+)
+
+echo ""
+echo "[way-to-algorithm-latex] xelatex wta.tex"
+xelatex wta.tex
+call:check_exit_code
+
+echo ""
+echo "[way-to-algorithm-latex] bibtex wta.aux"
+bibtex wta.aux
+call:check_exit_code
+
+echo ""
+echo "[way-to-algorithm-latex] xelatex wta.tex twice"
+xelatex wta.tex
+call:check_exit_code
+xelatex wta.tex
+call:check_exit_code
---
+++ clear.bat
@@ -0,0 +1,35 @@
+@echo off
+
+:quiet_rm
+:loop
+if "%1"=="" (
+    goto continue
+)
+if exist "%1\*" (
+    rd /S /Q %1
+)
+if not exist "%1\*" if exist %1 (
+    del %1
+)
+shift
+goto:loop
+:continue
+goto:eof
+
+echo ""
+echo "[way-to-algorithm-latex] remove binary files"
+call:quiet_rm *.dvi
+call:quiet_rm *.pdf
+call:quiet_rm *.ps
+call:quiet_rm *.aux
+call:quiet_rm *.log
+call:quiet_rm *.fdb_latexmk
+call:quiet_rm *.fls
+call:quiet_rm *.synctex*
+call:quiet_rm *.out
+call:quiet_rm *.thm
+call:quiet_rm *.toc
+call:quiet_rm *.toc
+call:quiet_rm __latexindent_temp.tex
+call:quiet_rm *.bbl
+call:quiet_rm *.blg
